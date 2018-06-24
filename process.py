c353261a2dc84d3215c277b6830256f9388de58c
ANR <anr@ANRs-MacBook-Pro.local>
2018-06-24 14:21:26 -0400

    nltk townload

--- process.py
+++ process.py
@@ -2,7 +2,8 @@ import nltk
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import stopwords
 import gensim
-# nltk.download('wordnet')
+nltk.download('wordnet')
+nltk.download('stopwords')
 lmtzr = WordNetLemmatizer()
 stopwordsList = stopwords.words('english')
 
